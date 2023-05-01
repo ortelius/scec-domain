39b18d2e8a662b23b6f4841b9f99c656845eba71
Steve Taylor <steve@deployhub.com>
2023-05-01 11:12:17 -0600

    add Zap logger Signed-off-by: Steve Taylor <steve@deployhub.com>

--- go.mod
+++ go.mod
@@ -5,16 +5,32 @@ go 1.20
 require (
 	github.com/arangodb/go-driver v1.5.2
 	github.com/gofiber/fiber/v2 v2.44.0
-	github.com/ortelius/scec-commons v1.0.3
+	github.com/ortelius/scec-commons v0.1.2
+	github.com/swaggo/swag v1.16.1
 )
 
 require (
+	github.com/swaggo/files/v2 v2.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/arangodb/go-velocypack v0.0.0-20200318135517-5af53c29c67e // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/gofiber/swagger v0.1.11
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/ipfs/go-cid v0.4.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -38,7 +54,7 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
 )
-
-replace github.com/ortelius/scec-commons => /Users/steve/git/xrpl/scec-commons
--- main.go
+++ main.go
@@ -1,134 +1,172 @@
+// Ortelius v11 Domain Microservice that handles creating and retrieving Domains
 package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net/http"
+
+	_ "cli/docs"
 
 	"github.com/arangodb/go-driver"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"github.com/ortelius/scec-commons/database"
 	"github.com/ortelius/scec-commons/model"
 )
 
+var logger = database.InitLogger()
 var dbconn = database.InitializeDB()
 
+// GetDomains godoc
+// @Summary Get a List of Domains
+// @Description Get a list of domains for the user.
+// @Tags domain
+// @Accept */*
+// @Produce json
+// @Success 200
+// @Router /msapi/domain [get]
 func GetDomains(c *fiber.Ctx) error {
 
-	parameters := map[string]interface{}{}
+	var cursor driver.Cursor       // db cursor for rows
+	var err error                  // for error handling
+	var ctx = context.Background() // use default database context
 
-	aql := `FOR domain in books
+	// query all the domains in the collection
+	aql := `FOR domain in evidence
 			RETURN domain`
 
-	ctx := driver.WithQueryCount(context.Background())
-	cursor, err := dbconn.Database.Query(ctx, aql, parameters)
-
-	if err != nil {
-		log.Fatalf("Failed to run query: %v", err)
+	// execute the query with no parameters
+	if cursor, err = dbconn.Database.Query(ctx, aql, nil); err != nil {
+		logger.Sugar().Errorf("Failed to run query: %v", err) // log error
 	}
 
-	defer cursor.Close()
+	defer cursor.Close() // close the cursor when returning from this function
 
-	var domains []model.Domain
+	var domains []model.Domain // define a list of domains to be returned
 
-	for {
-		var domain model.Domain
-		meta, err := cursor.ReadDocument(ctx, &domain)
-		if driver.IsNoMoreDocuments(err) {
-			break
-		} else if err != nil {
-			log.Fatalf("Failed to read document: %v", err)
+	for cursor.HasMore() { // loop thru all of the documents
+
+		var domain model.Domain      // fetched domain
+		var meta driver.DocumentMeta // data about the fetch
+
+		// fetch a document from the cursor
+		if meta, err = cursor.ReadDocument(ctx, &domain); err != nil {
+			logger.Sugar().Errorf("Failed to read document: %v", err)
 		}
-		domains = append(domains, domain)
-		fmt.Printf("Got doc with key '%s' from query\n", meta.Key)
+		domains = append(domains, domain)                                    // add the domain to the list
+		logger.Sugar().Infof("Got doc with key '%s' from query\n", meta.Key) // log the key
 	}
 
-	return c.JSON(domains)
+	return c.JSON(domains) // return the list of domains in JSON format
 }
 
+// GetDomain godoc
+// @Summary Get a Domain
+// @Description Get a domain based on the _key or name.
+// @Tags domain
+// @Accept */*
+// @Produce json
+// @Success 200
+// @Router /msapi/domain/:key [get]
 func GetDomain(c *fiber.Ctx) error {
 
-	key := c.Params("key")
-	parameters := map[string]interface{}{
+	var cursor driver.Cursor       // db cursor for rows
+	var err error                  // for error handling
+	var ctx = context.Background() // use default database context
+
+	key := c.Params("key")                // key from URL
+	parameters := map[string]interface{}{ // parameters
 		"key": key,
 	}
 
+	// query the domains that match the key or name
 	aql := `FOR domain in books
 			FILTER (domain.name == @key or domain._key == @key)
 			RETURN domain`
 
-	ctx := driver.WithQueryCount(context.Background())
-	cursor, err := dbconn.Database.Query(ctx, aql, parameters)
-
-	if err != nil {
-		log.Fatalf("Failed to run query: %v", err)
+	// run the query with patameters
+	if cursor, err = dbconn.Database.Query(ctx, aql, parameters); err != nil {
+		logger.Sugar().Errorf("Failed to run query: %v", err)
 	}
 
-	defer cursor.Close()
+	defer cursor.Close() // close the cursor when returning from this function
 
-	var domain model.Domain
+	var domain model.Domain // define a domain to be returned
 
-	if cursor.Count() == 0 { // not found so get from NFT Storage
-		key, cid2json := database.FetchFromLTS(key)
-		domain.Key = key
-		domain.UnmarshalNFT(cid2json)
-	} else {
-		for {
-			meta, err := cursor.ReadDocument(ctx, &domain)
-			if driver.IsNoMoreDocuments(err) {
-				break
-			} else if err != nil {
-				log.Fatalf("Failed to read document: %v", err)
-			}
-			fmt.Printf("Got doc with key '%s' from query\n", meta.Key)
+	if cursor.HasMore() { // domain found
+		var meta driver.DocumentMeta // data about the fetch
+
+		if meta, err = cursor.ReadDocument(ctx, &domain); err != nil {
+			logger.Sugar().Errorf("Failed to read document: %v", err)
 		}
+		logger.Sugar().Infof("Got doc with key '%s' from query\n", meta.Key)
+
+	} else { // not found so get from NFT Storage
+		key, cid2json := database.FetchFromLTS(key) // Use the CID to get the JSON from nft.storage
+		domain.Key = key                            // set the key for unmarshaling
+		domain.UnmarshalNFT(cid2json)               // convert the JSON to the domain object
 	}
-	return c.JSON(domain)
+
+	return c.JSON(domain) // return the domain in JSON format
 }
 
+// NewDomain godoc
+// @Summary Create a Domain
+// @Description Create a new Domain and persist it
+// @Tags domain
+// @Accept application/json
+// @Produce json
+// @Success 200
+// @Router /msapi/domain [post]
 func NewDomain(c *fiber.Ctx) error {
 
-	var err error
-	var meta driver.DocumentMeta
+	var err error                  // for error handling
+	var meta driver.DocumentMeta   // data about the document
+	var ctx = context.Background() // use default database context
+	domain := new(model.Domain)    // define a domain to be returned
 
-	domain := new(model.Domain)
-	if err = c.BodyParser(domain); err != nil {
+	if err = c.BodyParser(domain); err != nil { // parse the JSON into the domain object
 		return c.Status(503).Send([]byte(err.Error()))
 	}
 
-	cid2json := make(map[string]string, 0)
+	cid2json := make(map[string]string, 0) // create a map for normalizing the domain object into CIDs + JSON strings
+	nft := domain.MarshalNFT(cid2json)     // convert the domain object into a JSON for nft.storage
 
-	nft := domain.MarshalNFT(cid2json)
+	logger.Sugar().Infof("%+v\n", nft) // log the new nft
 
-	fmt.Printf("%+v\n", nft)
-
-	meta, err = dbconn.Collection.CreateDocument(dbconn.Context, domain)
-
-	if err != nil && !driver.IsConflict(err) {
-		log.Fatalf("Failed to create document: %v", err)
+	// add the domain to the database.  Ignore if it already exists since it will be identical
+	if meta, err = dbconn.Collection.CreateDocument(ctx, domain); err != nil && !driver.IsConflict(err) {
+		logger.Sugar().Errorf("Failed to create document: %v", err)
 	}
-	fmt.Printf("Created document in collection '%s' in db '%s' key='%s'\n", dbconn.Collection.Name(), dbconn.Database.Name(), meta.Key)
+	logger.Sugar().Infof("Created document in collection '%s' in db '%s' key='%s'\n", dbconn.Collection.Name(), dbconn.Database.Name(), meta.Key)
 
-	database.PersistOnLTS(cid2json)
+	database.PersistOnLTS(cid2json) // save the nft JSON version of the domain object to ntf.storage
 
-	return c.JSON(domain)
+	return c.JSON(domain) // return the domain object in JSON format.  This includes the new _key
 }
 
-func DeleteDomain(c *fiber.Ctx) error {
-	return c.Status(http.StatusForbidden).SendString("Deleting objects is not allowed")
-}
-
+// setupRoutes defines maps the routes to the functions
 func setupRoutes(app *fiber.App) {
 
-	app.Get("/msapi/domain", GetDomains)
-	app.Get("/msapi/domain/:key", GetDomain)
-	app.Post("/msapi/domain", NewDomain)
-	app.Delete("/msapi/domain/:key", DeleteDomain)
+	app.Get("/swagger/*", swagger.HandlerDefault) // handle displaying the swagger
+	app.Get("/msapi/domain", GetDomains)          // list of domains
+	app.Get("/msapi/domain/:key", GetDomain)      // single domain based on name or key
+	app.Post("/msapi/domain", NewDomain)          // save a single domain
 }
 
+// @title Ortelius v11 Domain Microservice
+// @version 11.0.0
+// @description RestAPI for the Domain Object
+// @termsOfService http://swagger.io/terms/
+// @contact.name Ortelius Google Group
+// @contact.email ortelius-dev@googlegroups.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:3000
+// @BasePath /msapi/domain
 func main() {
-	app := fiber.New()
-	setupRoutes(app)
-	app.Listen(":3000")
+	app := fiber.New()                          // create a new fiber application
+	setupRoutes(app)                            // define the routes for this microservice
+	if err := app.Listen(":3000"); err != nil { // start listening for incoming connections
+		logger.Sugar().Fatalf("Failed get the microservice running: %v", err)
+	}
 }
